dbfd03af5e7621c1aa693d98cb875c78b885beea
Amin Santos Nelli <104988818+amin-nelli@users.noreply.github.com>
2026-03-23 19:29:04 -0300

    Add files via upload

---
+++ Andre Iacono/List_Tuples_Dictionaries.py
@@ -0,0 +1,14 @@
+frutas = [] #lista
+diasSemana = ('segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado', 'domingo') #Tuplas
+
+frutas.append(input("digite um fruta: "))
+frutas.append(input("digite outra fruta: "))
+frutas.append(input("digite outra fruta: "))
+
+print('==== As Frutas Que Voce Listou Sao ===')
+for fruta in frutas:
+    print(fruta)
+
+
+for diaSemana in diasSemana:
+    print(diaSemana)
