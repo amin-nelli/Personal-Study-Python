d33ca088e4665c049e6dddf3ae92b065855ecde2
Amin Santos Nelli <104988818+amin-nelli@users.noreply.github.com>
2026-04-27 19:01:35 -0300

    Add files via upload

---
+++ Curso Em Video/Python3-M1/1_MédiaDoAluno.py
@@ -0,0 +1,5 @@
+m = float(input('Digite a nota de matematica: '))
+p = float(input('Digite a nota de portugues: '))
+g = float(input('Digite a nota de geografia: '))
+media = (m + p + g) / 3
+print(f'A média do aluno é: {media:.2f}')
---
+++ Curso Em Video/Python3-M1/3_ImportandoBiblioteca.py
@@ -0,0 +1,7 @@
+
+import math
+
+num1 = float(input('Digite um numero: '))
+
+raiz = math.sqrt(num1)
+print(f'A raiz quadrada de {num1} é {math.ceil(raiz)}')
